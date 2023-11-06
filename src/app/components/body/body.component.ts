d7c0ddfd4a516224d1d4969345f8f901a0d48057
Luis <Luis10amador@gmail.com>
2023-11-06 13:10:37 -0600

    -Finalizacion de la seccion

--- src/app/components/body/body.component.ts
+++ src/app/components/body/body.component.ts
@@ -6,6 +6,13 @@ import { Component } from '@angular/core';
 })
 
 export class BodyComponent {
+	mostrar = true;
 
+	frase :any = {
+		mensaje: 'Un gran poder conlleva una gran responsabilidad',
+		autor: 'Ben Parker'
+	};
+
+	personajes: string[] = ["Spidey","Venom","GreenGoblin"];
 
 }
